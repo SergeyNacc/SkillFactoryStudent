bc45d277c8f2a8f95ae7db5aab60faea116f8442
sergey_nacc <sergeynaksin@gmail.com>
2020-08-28 20:42:09 +0700

    firs commit

---
+++ src/students/Student.java
@@ -0,0 +1,33 @@
+package students;
+
+public class Student {
+
+    private String name;
+    private String surname;
+    private int age;
+
+    private int group;
+    private int completedTask;
+    private int completedModule;
+    private boolean experience;
+
+
+    public Student(String name, String surname, int age) {
+        this(name, surname, age,
+                group: 0,
+                completedTask: 0,
+                completedModule: 0,
+                experience: false);
+    }
+
+    public Student(String name, String surname, int age,
+                   int group, int completedTask, int completedModule, boolean experience) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.group = group;
+        this.completedTask = completedTask;
+        this.completedModule = completedModule;
+        this.experience = experience;
+    }
+}
