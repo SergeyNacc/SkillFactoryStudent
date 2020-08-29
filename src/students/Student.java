39577aebd1dadb75d4b56c778ac9c9755f5ac4b1
sergey_nacc <sergeynaksin@gmail.com>
2020-08-29 14:18:06 +0700

    add class Person

--- src/students/Student.java
+++ src/students/Student.java
@@ -11,6 +11,9 @@ public class Student {
     private int completedModule;
     private boolean experience;
 
+    public static int taskCountForAll;
+    public static final int MAX_MODULE_COUNT = 20;
+
 
     public Student(String name, String surname, int age) {
         this(name, surname, age, 0, 0, 0, false);
@@ -25,5 +28,22 @@ public class Student {
         this.completedTask = completedTask;
         this.completedModule = completedModule;
         this.experience = experience;
+    }
+
+
+    public void solveTask() {
+        System.out.println("Задание решено!");
+        completedTask++;
+        taskCountForAll++;
+    }
+
+
+    public void passModule() {
+        if(completedModule < MAX_MODULE_COUNT) {
+            completedModule++;
+            System.out.println("Модуль пройден!");
+        } else {
+            System.out.println("Все модули пройдены!");
+        }
     }
 }
